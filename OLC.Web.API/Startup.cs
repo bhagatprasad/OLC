39ebf6a476e7fcad4cd32b1b1a9530e89db03d60
bhagatprasad <bhagatprasad8@yahoo.com>
2025-10-12 08:51:00 +0530

    commit

---
+++ OLC.Web.API/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace OLC.Web.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+
+            services.AddMvc().AddXmlSerializerFormatters();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "OLC.Web.API",
+                    Description = "OLC.Web.API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "OLC.Web.API Service",
+                        Email = "contact@bdprasad.in",
+                        Url = new Uri("https://bdprasad.in")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "contact@bdprasad.in",
+                        Url = new Uri("https://bdprasad.in")
+                    }
+
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            //app.UseAuthentication();
+
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+            });
+
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OLC.Web.API");
+            });
+
+        }
+    }
+}
