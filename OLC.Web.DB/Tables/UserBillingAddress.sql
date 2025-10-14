0ce8b959b39cf75b85b9005367be115bbed1c8d4
bhagatprasad <bhagatprasad8@yahoo.com>
2025-10-14 21:25:39 +0530

    commit

--- OLC.Web.DB/Tables/UserBillingAddress.sql
+++ OLC.Web.DB/Tables/UserBillingAddress.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[UserBillingAddress]
 (
 	[Id]                            bigint                 NOT NULL    PRIMARY KEY   identity(1,1),
+	[UserId]                        bigint                 NOT NULL,
 	[AddessLineOne]                 varchar(max)           NULL,
 	[AddessLineTwo]                 varchar(max)           NULL,
 	[AddessLineThress]              varchar(max)           NULL,
 	[Location]                      varchar(max)           NULL,
 	[CountryId]                     bigint                 NULL,
 	[StateId]                       bigint                 NULL,
-	[CityId]                          bigint				   NULL,
+	[CityId]                        bigint				   NULL,
 	[PinCode]						varchar(max)           NULL,
 	[CreatedBy]                     bigint                 NULL,
 	[CreatedOn]                     datetimeoffset         NULL,
