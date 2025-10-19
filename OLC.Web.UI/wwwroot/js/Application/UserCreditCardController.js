e16159fe1c60be0e04e131ac723495991587e5e8
bhagatprasad <bhagatprasad8@yahoo.com>
2025-10-19 12:56:30 +0530

    inital UI Stuff for sidebar

---
+++ OLC.Web.UI/wwwroot/js/Application/UserCreditCardController.js
@@ -0,0 +1,24 @@
+﻿function UserCreditCardController() {
+    var self = this;
+    self.init = function () {
+
+        $(document).on("click", "#addCreditCardBtn", function () {
+            $('#sidebar').addClass('show');
+            $('body').append('<div class="modal-backdrop fade show"></div>');
+            console.log("Iam getting from add button click");
+        });
+
+
+        $(document).on("click", "#closeSidebar", function () {
+            $('#AddEditUserCreditCardForm')[0].reset();
+            $('#sidebar').removeClass('show');
+            $('.modal-backdrop').remove();
+        });
+
+        $('#AddEditUserCreditCardForm').on('submit', function (e) {
+            e.preventDefault();
+            showLoader();
+            console.log(userRegistration);
+        });
+    }
+}
