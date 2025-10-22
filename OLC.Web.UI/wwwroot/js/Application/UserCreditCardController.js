50496b4df61297bee2dba8104469a3986796430b
swaroopa649 <kaviriswaroopa1996@gmail.com>
2025-10-22 11:27:28 +0530

    commit_CreditCard_Integration

--- OLC.Web.UI/wwwroot/js/Application/UserCreditCardController.js
+++ OLC.Web.UI/wwwroot/js/Application/UserCreditCardController.js
@@ -8,7 +8,6 @@
             console.log("Iam getting from add button click");
         });
 
-
         $(document).on("click", "#closeSidebar", function () {
             $('#AddEditUserCreditCardForm')[0].reset();
             $('#sidebar').removeClass('show');
