53545f217be083d3c4816a3576f7a366a0c14387
vamshigogu <vamshi.gogu@yahoo.com>
2025-10-24 20:43:46 +0530

    Created State API integration

---
+++ OLC.Web.UI/Services/StateService.cs
@@ -0,0 +1,31 @@
+﻿using OLC.Web.UI.Models;
+
+namespace OLC.Web.UI.Services
+{
+    public class StateService:IStateService
+    {
+        private readonly IRepositoryFactory _repositoryFactory;
+        public StateService(IRepositoryFactory repositoryFactory)
+        {
+            _repositoryFactory = repositoryFactory;
+        }
+
+        public async Task<State> GetStateByStateAsync(long stateId)
+        {
+            var url = Path.Combine("State/GetStateByStateAsync", stateId.ToString());
+            return await _repositoryFactory.SendAsync<State>(HttpMethod.Get,url);
+            
+        }
+
+        public async Task<List<State>> GetStatesByCountryAsync(long countryId)
+        {
+            var url = Path.Combine("State/GetStatesByCountryAsync", countryId.ToString());
+            return await _repositoryFactory.SendAsync<List<State>>(HttpMethod.Get,url);
+        }
+
+        public async Task<List<State>> GetStatesListAsync()
+        {
+            return await _repositoryFactory.SendAsync<List<State>>(HttpMethod.Get, "State/GetStatesListAsync");
+        }
+    }
+}
