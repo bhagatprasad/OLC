18935ceae2c8cdbe98688c4d93cf5fb5a81e4575
vamshigogu <vamshi.gogu@yahoo.com>
2025-11-16 12:03:22 +0530

    UserKycDocument procedures and APIs

--- OLC.Web.API/Controllers/UserController.cs
+++ OLC.Web.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OLC.Web.API.Manager;
 using OLC.Web.API.Models;
+using System.Runtime.InteropServices;
 
 namespace OLC.Web.API.Controllers
 {
@@ -11,10 +12,12 @@ namespace OLC.Web.API.Controllers
     {
         private readonly IUserManager _userManager;
         private readonly IUserKycManager _userKycManager;
-        public UserController(IUserManager userManager,IUserKycManager userKycManager)
+        private readonly IUserKycDocumentManager _userKycDocumentManager;
+        public UserController(IUserManager userManager,IUserKycManager userKycManager, IUserKycDocumentManager userKycDocumentManager)
         {
             _userManager = userManager;
-            _userKycManager = userKycManager; 
+            _userKycManager = userKycManager;
+            _userKycDocumentManager = userKycDocumentManager;
         }
         [HttpGet]
         [Route("GetUserAccountsAsync")]
@@ -89,6 +92,49 @@ namespace OLC.Web.API.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
+        [HttpPost]
+        [Route("UploadeUserKycDocumenAsync")]
+        public async Task<IActionResult> UploadeUserKycDocumenAsync(UserKycDocument userKycDocument)
+        {
+            try
+            {
+                var response = await _userKycDocumentManager.UploadeUserKycDocumentAsync(userKycDocument);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
 
+        [HttpPost]
+        [Route("UpdateUserKycDocumenAsync")]
+        public async Task<IActionResult> UpdateUserKycDocumenAsync(UserKycDocument userKycDocument)
+        {
+            try
+            {
+                var response = await _userKycDocumentManager.UpdateUserKycDocumentAsync(userKycDocument);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetAllUsersKycDocuments")]
+        public async Task<IActionResult> GetAllUsersKycDocuments()
+        {
+            try
+            {
+                var response = await _userKycDocumentManager.GetAllUsersKycDocuments();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
---
+++ OLC.Web.API/Models/UserKycDocument.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace OLC.Web.API.Models
+{
+    public class UserKycDocument
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public string? DocumentFilePath { get; set; }
+        public byte[]? DocumentFileData { get; set; }
+        public string VerificationStatus { get; set; }
+        public DateTimeOffset? VerifiedOn { get; set; }
+        public long? VerifiedBy { get; set; }
+        public string? RejectionReason { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public long? CreatedBy { get; set; }
+        public DateTimeOffset? CreatedOn { get; set; }
+        public long? ModifiedBy { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
