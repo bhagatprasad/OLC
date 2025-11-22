7b71410393bc4d7c9dbc53060b983be08b53c458
bhagatprasad <bhagatprasad8@yahoo.com>
2025-11-23 00:20:18 +0530

    commit

--- OLC.Web.DB/Programmability/Stored Procedures/UserWallet/uspUpdateUserWalletBalance.sql
+++ OLC.Web.DB/Programmability/Stored Procedures/UserWallet/uspUpdateUserWalletBalance.sql
@@ -1,45 +1,20 @@
-﻿CREATE PROCEDURE [dbo].[uspUpdateUserWalletBalance]
+﻿CREATE PROCEDURE [dbo].[uspUpdateUSerWalletBalance]
 (
-    @WalletId BIGINT,
-    @RewardAmount DECIMAL(18,6),
-    @ModifiedBy BIGINT,
-    @TransactionType NVARCHAR(20) = 'Credit',
-    @Description NVARCHAR(255) = NULL,
-    @ReferenceId NVARCHAR(20) = NULL
+	@userId			nvarchar(20),
+	@currentBalance		DECIMAL(18,6),
+	@totalEarned		DECIMAL(18,6),
+	@totalSpent			DECIMAL(18,6)
 )
 AS
 BEGIN
-    SET NOCOUNT ON;
-
-    DECLARE @ModifiedOn DATETIMEOFFSET = GETUTCDATE();
-    DECLARE @UserId BIGINT;
-
-    -- Get UserId from wallet
-    SELECT @UserId = UserId 
-    FROM [dbo].[UserWallet] 
-    WHERE [Id] = @WalletId;
-
-    IF @UserId IS NULL
-    BEGIN
-        RAISERROR('Wallet not found.', 16, 1);
-        RETURN -1;
-    END
-
-    -- Update wallet balance
-    UPDATE [dbo].[UserWallet]
-    SET 
-        [CurrentBalance] = [CurrentBalance] + @RewardAmount,
-        [TotalEarned] = [TotalEarned] + @RewardAmount,
-        [ModifiedOn] = @ModifiedOn,
-        [ModifiedBy] = @ModifiedBy
-    WHERE [Id] = @WalletId;
-
-    -- Insert wallet log
-    EXEC [dbo].[uspInsertUserWalletLog]
-        @WalletId = @WalletId,
-        @UserId = @UserId,
-        @Amount = @RewardAmount,
-        @TransactionType = @TransactionType,
-        @Description = @Description,
-        @ReferenceId = @ReferenceId;
+	UPDATE [dbo].[UserWallet] 
+	SET
+	CurrentBalance	= @currentBalance,
+	TotalEarned		= @totalEarned,
+	TotalSpent		= @totalSpent,
+	ModifiedON		=GETDATE()
+WHERE 
+	UserId	=@userId
 END
+	
+		
