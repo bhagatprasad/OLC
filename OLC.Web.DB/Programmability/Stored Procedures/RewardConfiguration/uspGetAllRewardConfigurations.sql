1308f72930be9fed2ff2078b08cf9b280ae03ff2
soyamraghu <soyamraghu65@gmail.com>
2025-11-26 16:01:11 +0530

    RewardCopnfiguration_API_Implementation

---
+++ OLC.Web.DB/Programmability/Stored Procedures/RewardConfiguration/uspGetAllRewardConfigurations.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[uspGetAllRewardConfigurations]  
+AS  
+BEGIN  
+    SET NOCOUNT ON;  
+  
+    SELECT   
+        Id,  
+        RewardName,  
+        RewardType,  
+        RewardValue,  
+        MinimumTransactionAmount,  
+        MaximumReward,  
+        IsActive,  
+        ValidFrom,  
+        ValidTo,  
+        CreatedBy,  
+        CreatedOn,  
+        ModifiedBy,  
+        ModifiedOn  
+    FROM   
+        [dbo].[RewardConfiguration]  
+    ORDER BY   
+        CreatedOn DESC;   
+END  
---
+++ OLC.Web.DB/Programmability/Stored Procedures/RewardConfiguration/uspGetRewardConfigurationDetailsById.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[uspGetRewardConfigurationDetailsById]  
+(  
+    @Id BIGINT  
+)  
+AS  
+BEGIN  
+    SET NOCOUNT ON;  
+  
+    SELECT   
+        Id,  
+        RewardName,  
+        RewardType,  
+        RewardValue,  
+        MinimumTransactionAmount,  
+        MaximumReward,  
+        IsActive,  
+        ValidFrom,  
+        ValidTo,  
+        CreatedBy,  
+        CreatedOn,  
+        ModifiedBy,  
+        ModifiedOn  
+    FROM [dbo].[RewardConfiguration]   
+    WHERE Id = @Id;  
+END  
