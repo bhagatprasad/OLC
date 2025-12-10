6c0f2a1fb52157af587ce370adeb635a351d48b5
vamshigogu <vamshi.gogu@yahoo.com>
2025-12-10 19:48:46 +0530

    Created Email Category APIs And Procedures

---
+++ OLC.Web.DB/Tables/EmailCategory.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[EmailCategory]
+(
+[Id]					BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[Name]					NVARCHAR(200)		 NOT NULL,
+[Code]					NVARCHAR (100)		null,
+[CreatedBy]             BIGINT              NULL,
+[CreatedOn]             DATETIMEOFFSET      NULL DEFAULT GETDATE(),
+[ModifiedBy]            BIGINT              NULL,
+[ModifiedOn]            DATETIMEOFFSET      NULL DEFAULT GETDATE(),
+[IsActive]              BIT                 NULL DEFAULT 1
+)
