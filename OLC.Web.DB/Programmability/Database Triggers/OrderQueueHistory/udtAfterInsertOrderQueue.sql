e8a15494962501981f68db07da59fa3593089f6f
soyamraghu <soyamraghu65@gmail.com>
2025-12-17 13:47:50 +0530

    OrderQueue_AfterTrigger_Implementation

---
+++ OLC.Web.DB/Programmability/Database Triggers/OrderQueueHistory/udtAfterInsertOrderQueue.sql
@@ -0,0 +1,52 @@
+﻿CREATE TRIGGER [dbo].[udtAfterInsertOrderQueue]
+ON [dbo].[OrderQueue]
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[OrderQueueHistory]
+    (
+        OrderQueueId,
+        PaymentOrderId,
+        ExecutiveId,
+        OrderReference,
+        Priority,
+        AssignedOn,
+        ProcessingStartedOn,
+        ProcessingCompletedOn,
+        QueueStatus,
+        RetryCount,
+        FailureReason,
+        Metadata,
+        Description,
+        ActionType,
+        CreatedBy,
+        CreatedOn,
+        ModifiedBy,
+        ModifiedOn,
+        IsActive
+    )
+    SELECT
+        i.Id                          AS OrderQueueId,
+        i.PaymentOrderId,
+        i.AssignedExecutiveId         AS ExecutiveId,
+        i.OrderReference,
+        i.Priority,
+        i.AssignedOn,
+        i.ProcessingStartedOn,
+        i.ProcessingCompletedOn,
+        i.QueueStatus,
+        i.RetryCount,
+        i.FailureReason,
+        i.Metadata,
+        'Order created in queue'      AS Description,
+        'INSERT'                      AS ActionType,
+        i.CreatedBy,
+        i.CreatedOn,
+        i.ModifiedBy,
+        i.ModifiedOn,
+        i.IsActive
+    FROM inserted i;
+END;
+GO
---
+++ OLC.Web.DB/Tables/OrderQueueHistory.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[OrderQueueHistory]
+(
+    [Id]                       BIGINT IDENTITY(1,1) PRIMARY KEY,  
+    [OrderQueueId]             BIGINT               NOT NULL,
+    [PaymentOrderId]           BIGINT               NULL,
+	[ExecutiveId]              BIGINT               NULL,
+    [OrderReference]           NVARCHAR(50)         NULL, 
+	[Priority]                 INT                  DEFAULT 5                     NOT NULL,
+	[AssignedOn]               DATETIMEOFFSET       NULL,
+    [ProcessingStartedOn]      DATETIMEOFFSET       NULL,
+    [ProcessingCompletedOn]    DATETIMEOFFSET       NULL,
+    [QueueStatus]              NVARCHAR(20)         NULL,
+	[RetryCount]               INT                  NULL,	
+	[FailureReason]            NVARCHAR(MAX)        NULL,    
+    [Metadata]                 NVARCHAR(MAX)        NULL,
+    [Description]              NVARCHAR(255)        NULL,
+	[ActionType]               NVARCHAR(30)         NOT NULL, -- INSERT, UPDATE, STATUS_CHANGE, ASSIGN, RETRY, COMPLETE, FAIL 
+    [CreatedBy]                BIGINT               NULL,
+    [CreatedOn]                DATETIMEOFFSET       NULL DEFAULT GETDATE(),
+    [ModifiedOn]               DATETIMEOFFSET       NULL DEFAULT GETDATE(),
+    [ModifiedBy]               BIGINT               NULL,
+    [IsActive]                 BIT                  NOT NULL DEFAULT 1  
+);
