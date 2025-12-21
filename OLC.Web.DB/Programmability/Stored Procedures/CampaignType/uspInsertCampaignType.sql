c31c71698c2a0879c26c27b3f68f9ccff93c807f
vamshigogu <vamshi.gogu@yahoo.com>
2025-12-21 09:44:20 +0530

    Campaign type API and UI

---
+++ OLC.Web.DB/Programmability/Stored Procedures/CampaignType/uspInsertCampaignType.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[uspInsertCampaignType]
+(
+    @Name               VARCHAR(MAX),
+    @Code               VARCHAR(10),
+    @Description        NVARCHAR(500),
+    @CreatedBy          BIGINT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[CampaignType]
+    (
+        [Name],
+        [Code],
+        [Description],
+        [CreatedBy],
+        [CreatedOn],
+        [ModifiedBy],
+        [ModifiedOn],
+        [IsActive]
+
+    )
+    VALUES
+    (
+        @Name,
+        @Code,
+        @Description,
+        @CreatedBy,
+        GETDATE(),
+        @CreatedBy,
+        GETDATE(),
+        1
+    );
+END;
+
